049f206fc6504337494aa3238465d1add6730f83
Rahix <rahix@rahix.de>
2022-06-06 16:46:04 +0200

    Implement i2c proxy for embedded-hal 1.0.0-alpha.8
    Some drivers and HALs have already started using the embedded-hal alpha
    releases so it seems we really need to add support for them now.
    
    As a starting point, implement i2c sharing ontop of the blocking I2C
    trait from the embedded-hal alpha 8.

--- src/proxies.rs
+++ src/proxies.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "eh-alpha")]
+use embedded_hal_alpha::i2c as i2c_alpha;
+
 use embedded_hal::adc;
 use embedded_hal::blocking::i2c;
 use embedded_hal::blocking::spi;
@@ -58,6 +61,76 @@ where
     ) -> Result<(), Self::Error> {
         self.mutex
             .lock(|bus| bus.write_read(addr, buffer_in, buffer_out))
+    }
+}
+
+// Implementations for the embedded_hal alpha
+
+#[cfg(feature = "eh-alpha")]
+impl<'a, M: crate::BusMutex> i2c_alpha::ErrorType for I2cProxy<'a, M>
+where
+    M::Bus: i2c_alpha::ErrorType,
+{
+    type Error = <M::Bus as i2c_alpha::ErrorType>::Error;
+}
+
+#[cfg(feature = "eh-alpha")]
+impl<'a, M: crate::BusMutex> i2c_alpha::blocking::I2c for I2cProxy<'a, M>
+where
+    M::Bus: i2c_alpha::blocking::I2c,
+{
+    fn read(&mut self, address: u8, buffer: &mut [u8]) -> Result<(), Self::Error> {
+        self.mutex.lock(|bus| bus.read(address, buffer))
+    }
+
+    fn write(&mut self, address: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+        self.mutex.lock(|bus| bus.write(address, bytes))
+    }
+
+    fn write_iter<B>(&mut self, address: u8, bytes: B) -> Result<(), Self::Error>
+    where
+        B: IntoIterator<Item = u8>,
+    {
+        self.mutex.lock(|bus| bus.write_iter(address, bytes))
+    }
+
+    fn write_read(
+        &mut self,
+        address: u8,
+        bytes: &[u8],
+        buffer: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.mutex
+            .lock(|bus| bus.write_read(address, bytes, buffer))
+    }
+
+    fn write_iter_read<B>(
+        &mut self,
+        address: u8,
+        bytes: B,
+        buffer: &mut [u8],
+    ) -> Result<(), Self::Error>
+    where
+        B: IntoIterator<Item = u8>,
+    {
+        self.mutex
+            .lock(|bus| bus.write_iter_read(address, bytes, buffer))
+    }
+
+    fn transaction<'b>(
+        &mut self,
+        address: u8,
+        operations: &mut [i2c_alpha::blocking::Operation<'b>],
+    ) -> Result<(), Self::Error> {
+        self.mutex.lock(|bus| bus.transaction(address, operations))
+    }
+
+    fn transaction_iter<'b, O>(&mut self, address: u8, operations: O) -> Result<(), Self::Error>
+    where
+        O: IntoIterator<Item = i2c_alpha::blocking::Operation<'b>>,
+    {
+        self.mutex
+            .lock(|bus| bus.transaction_iter(address, operations))
     }
 }
 
